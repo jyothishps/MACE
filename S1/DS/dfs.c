42b4603f34cf4f0940636760e9045a26eaf9f41c
Jyothish P S <psjyothish07@gmail.com>
2025-11-17 10:44:45 +0530

    Add DFS implementation in dfs.c
    Implement Depth First Search (DFS) algorithm with user input for adjacency matrix and starting vertex.

---
+++ S1/DS/dfs.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#define MAX 100
+
+int visited[MAX];
+int adj[MAX][MAX];
+int n;
+
+void DFS(int vertex) {
+    int i;
+    visited[vertex] = i;
+    printf("%d ",vertex);
+    for(i=0;i<n;i++) {
+        if(adj[vertex][i] == 1 && !visited[i]) {
+            DFS(i);
+        }
+    }
+}
+
+int main() {
+    int i,j;
+    int start;
+
+    printf("Enter no. of vertices: ");
+    scanf("%d",&n);
+
+    printf("Enter adjacency matrix (0 or 1):\n");
+    for(i=0;i<n;i++) {
+        for(j=0;j<n;j++) {
+            scanf("%d",&adj[i][j]);
+        }
+    }
+
+    for(i=0;i<n;i++)
+        visited[i] = 0;
+
+    printf("Enter starting vertex (0 - %d): ",n-1);
+    scanf("%d",&start);
+
+    printf("DFS Traversal starting from %d:\n",start);
+    DFS(start);
+
+    printf("\n");
+    return 0;
+}
