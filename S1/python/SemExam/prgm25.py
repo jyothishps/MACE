4261d96e3d427b57921a1c7798824122878f0abe
jyothishps <psjyothish07@gmail.com>
2026-01-11 17:12:26 +0530

    no. of strings with length 2 or more

---
+++ S1/python/SemExam/prgm25.py
@@ -0,0 +1,13 @@
+# Count the no. of strings where string length is 2 or more and the first and last characters are same
+
+def count_strings(lst):
+    count = 0
+    for w in lst:
+        if len(w)>=2 and w[0]==w[-1]:
+            count+=1
+    return count
+lst = input("Enter strings:").split(" ")
+print("No. of strings where string length is 2 or more and first and last characters are same:",count_strings(lst))
+
+# Enter strings:level apple malayalam
+# No. of strings where string length is 2 or more and first and last characters are same: 2
